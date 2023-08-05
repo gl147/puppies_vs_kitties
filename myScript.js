0012ec551ed418ef3bf340f6851d00f0e21f9261
gl147 <mossvova1@gmail.com>
2023-08-04 18:20:56 -0700

    added check_colision function, need to be done: puppie stucks between 0 and -10 px margin left zone

--- myScript.js
+++ myScript.js
@@ -8,24 +8,64 @@ let interval;
 
 puppie.addEventListener("click", (e) => {
   let direction = e.target.classList[1];
+  correct_moving(direction);
+});
 
+let correct_moving = (direction) => {
   if (isMoving) {
-    clearInterval(interval),
-      (interval = setInterval(
-        () => moving(direction, margin_top, margin_left),
-        1000
-      ));
+    clearInterval(interval);
+    interval = setInterval(
+      () => moving(direction, margin_top, margin_left),
+      1000
+    );
   } else {
     interval = setInterval(
       () => moving(direction, margin_top, margin_left),
       1000
     );
-    isMoving = true;
+    return (isMoving = true);
   }
-});
+};
+
+let check_collision = (direction, mt, ml) => {
+  let new_direction = direction;
+  if (mt < 0) {
+    switch (direction) {
+      case "left_top":
+        return (new_direction = "left_bot");
+      case "right_top":
+        return (new_direction = "right_bot");
+    }
+  } else if (mt > 800) {
+    switch (direction) {
+      case "left_bot":
+        return (new_direction = "left_top");
+      case "right_bot":
+        return (new_direction = "right_top");
+    }
+  } else if (ml < 0) {
+    switch (direction) {
+      case "left_top":
+        console.log(direction);
+        return (new_direction = "right_top");
+      case "left_bot":
+        console.log(direction);
+        return (new_direction = "right_bot");
+    }
+  } else if (ml > 1200) {
+    switch (direction) {
+      case "right_top":
+        return (new_direction = "left_top");
+      case "right_bot":
+        return (new_direction = "left_bot");
+    }
+  } else return new_direction;
+};
 
 let moving = (direction, mt, ml) => {
-  switch (direction) {
+  console.log(mt, ml);
+  let new_direction = check_collision(direction, mt, ml);
+  switch (new_direction) {
     case "right_top":
       mt -= 10;
       ml += 10;
