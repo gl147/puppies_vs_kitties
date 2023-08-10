7c02424e51ef5fca7bf310fe3d4e1f01e86fa706
gl147 <mossvova1@gmail.com>
2023-08-09 17:19:01 -0700

    fixed stucking at edge, speed up, need to be done: add kittie, add explosion when when kittie touch puppie, add puppies and kitties by buttons

--- myScript.js
+++ myScript.js
@@ -5,67 +5,68 @@ let margin_left = 0;
 
 let isMoving = false;
 let interval;
+let direction;
 
 puppie.addEventListener("click", (e) => {
-  let direction = e.target.classList[1];
+  direction = e.target.classList[1];
   correct_moving(direction);
 });
 
-let correct_moving = (direction) => {
+let correct_moving = (dr) => {
   if (isMoving) {
     clearInterval(interval);
     interval = setInterval(
       () => moving(direction, margin_top, margin_left),
-      1000
+      10
     );
   } else {
     interval = setInterval(
       () => moving(direction, margin_top, margin_left),
-      1000
+      10
     );
     return (isMoving = true);
   }
 };
 
-let check_collision = (direction, mt, ml) => {
-  let new_direction = direction;
+let check_collision = (dr, mt, ml) => {
+  let new_dr = dr;
   if (mt < 0) {
-    switch (direction) {
+    switch (dr) {
       case "left_top":
-        return (new_direction = "left_bot");
+        return (new_dr = "left_bot");
       case "right_top":
-        return (new_direction = "right_bot");
+        return (new_dr = "right_bot");
     }
-  } else if (mt > 800) {
-    switch (direction) {
+  } else if (mt > 610) {
+    switch (dr) {
       case "left_bot":
-        return (new_direction = "left_top");
+        return (new_dr = "left_top");
       case "right_bot":
-        return (new_direction = "right_top");
+        return (new_dr = "right_top");
     }
   } else if (ml < 0) {
-    switch (direction) {
+    switch (dr) {
       case "left_top":
         console.log(direction);
-        return (new_direction = "right_top");
+        return (new_dr = "right_top");
       case "left_bot":
         console.log(direction);
-        return (new_direction = "right_bot");
+        return (new_dr = "right_bot");
     }
-  } else if (ml > 1200) {
-    switch (direction) {
+  } else if (ml > 1010) {
+    switch (dr) {
       case "right_top":
-        return (new_direction = "left_top");
+        return (new_dr = "left_top");
       case "right_bot":
-        return (new_direction = "left_bot");
+        return (new_dr = "left_bot");
     }
-  } else return new_direction;
+  } else return new_dr;
 };
 
-let moving = (direction, mt, ml) => {
+let moving = (dr, mt, ml) => {
   console.log(mt, ml);
-  let new_direction = check_collision(direction, mt, ml);
-  switch (new_direction) {
+  direction = check_collision(dr, mt, ml);
+  switch (direction) {
     case "right_top":
       mt -= 10;
       ml += 10;
